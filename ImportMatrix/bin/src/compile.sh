a7ba7ef8e5b1be70edaa53a1905149d11284b2ad
RitchieLabIGH <45385836+RitchieLabIGH@users.noreply.github.com>
2019-06-18 11:31:55 +0200

    Update compile.sh

--- ImportMatrix/bin/src/compile.sh
+++ ImportMatrix/bin/src/compile.sh
@@ -3,7 +3,8 @@ g++ -std=c++0x -O3 -fopenmp RemoveNonInformativeKmers.cpp -o NIK
 g++ -std=c++0x -O3 -fopenmp JoinMultiImportationFiles.cpp -o JoinKMers
 g++ -std=c++0x -O3 -fopenmp JoinUltraBigMatrix.cpp -o JoinKmersUltra
 g++ -std=c++0x -O3 -fopenmp transformIntoMLformat.cpp -o transformIntoML
+g++ -std=c++0x -O3 -fopenmp CutMatrixByClass.cpp -o CutMatrixByClass
 g++ -std=c++0x -O3 MLformatToBinary.cpp -lm -lz -o transformIntoBinary
 g++ -std=c++0x -O3 MLformatToBinaryRemoveGroup.cpp -lm -lz -o transformIntoBinaryRemoveGroup
 make -f ./anova_makefile
-mv anovaFilter indexingKmers NIK JoinKmersUltra JoinKMers transformIntoML transformIntoBinary transformIntoBinaryRemoveGroup ../.
+mv anovaFilter indexingKmers NIK JoinKmersUltra JoinKMers transformIntoML transformIntoBinary transformIntoBinaryRemoveGroup CutMatrixByClass ../.
